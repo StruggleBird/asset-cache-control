a9968364c58005d28b9af6e2eb871e0560184575
Ternence <mjhx_zt@qq.com>
2015-01-19 11:41:47 +0800

    解决将相当路径误改为绝对路径的问题

--- src/main/java/org/zt/cachecontrol/ResourceVersionMojo.java
+++ src/main/java/org/zt/cachecontrol/ResourceVersionMojo.java
@@ -21,130 +21,125 @@ import org.apache.maven.plugin.MojoFailureException;
  */
 public class ResourceVersionMojo extends ResourceMojo {
 
-	public ResourceVersionMojo() {
+    public ResourceVersionMojo() {
 
-	}
+    }
 
-	@Override
-	public void execute() throws MojoExecutionException, MojoFailureException {
-		super.execute();
-		try {
-			versionPageUrl();
-		} catch (IOException e) {
-			throw new MojoExecutionException("Version url exception:", e);
-		}
-	}
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        super.execute();
+        try {
+            versionPageUrl();
+        } catch (IOException e) {
+            throw new MojoExecutionException("Version url exception:", e);
+        }
+    }
 
-	private void versionPageUrl() throws IOException {
+    private void versionPageUrl() throws IOException {
 
-		for (String pageDir : pageDirs) {
-			String absDir = webappDir + File.separator + pageDir;
-			File f = new File(absDir);
-			iterateDir(f);
-		}
-	}
+        for (String pageDir : pageDirs) {
+            String absDir = webappDir + File.separator + pageDir;
+            File f = new File(absDir);
+            iterateDir(f);
+        }
+    }
 
-	// 遍历处理
-	private void iterateDir(File file) throws IOException {
-		if (file.isDirectory()) {
-			File[] fs = file.listFiles();
-			for (File f : fs) {
-				iterateDir(f);
-			}
-		} else {
-			String fileName = file.getName();
-			String sufix = FilenameUtils.getExtension(fileName);
-			if (StringUtils.isNotEmpty(sufix)
-					&& ArrayUtils.contains(suffixs, sufix.toLowerCase())) {
-				log.info("processing file:" + file.getAbsolutePath());
-				String pageText = FileUtils.readFileToString(file, "UTF-8");
-				pageText = handleFile(pageText);
-				FileUtils.writeStringToFile(file, pageText, "UTF-8");
-			}
+    // 遍历处理
+    private void iterateDir(File file) throws IOException {
+        if (file.isDirectory()) {
+            File[] fs = file.listFiles();
+            for (File f : fs) {
+                iterateDir(f);
+            }
+        } else {
+            String fileName = file.getName();
+            String sufix = FilenameUtils.getExtension(fileName);
+            if (StringUtils.isNotEmpty(sufix) && ArrayUtils.contains(suffixs, sufix.toLowerCase())) {
+                log.info("processing file:" + file.getAbsolutePath());
+                String pageText = FileUtils.readFileToString(file, "UTF-8");
+                pageText = handleFile(pageText);
+                FileUtils.writeStringToFile(file, pageText, "UTF-8");
+            }
 
-		}
+        }
 
-	}
+    }
 
-	public String handleFile(String page) {
-		page = resolveJsRefer(page);
-		page = resolveCssRefer(page);
-		page = resolveImageRefer(page);
-		return page;
-	}
+    public String handleFile(String page) {
+        page = resolveJsRefer(page);
+        page = resolveCssRefer(page);
+        page = resolveImageRefer(page);
+        return page;
+    }
 
-	public String resolveJsRefer(String page) {
-		Matcher matcher = JS_PATTERN.matcher(page);
-		StringBuffer sb = new StringBuffer();
+    public String resolveJsRefer(String page) {
+        Matcher matcher = JS_PATTERN.matcher(page);
+        StringBuffer sb = new StringBuffer();
 
-		while (true) {
-			if (!matcher.find()) {
-				break;
-			}
-			String pfull = matcher.group();
-			String jsdirPart = matcher.group(1);
-			pfull = pfull.substring(0, matcher.start(1) - matcher.start())
-					+ resolveStaticAddr(jsdirPart.trim())
-					+ pfull.charAt(pfull.length() - 1);
-			matcher.appendReplacement(sb, Matcher.quoteReplacement(pfull));
-		}
-		matcher.appendTail(sb);
-		return sb.toString();
-	}
+        while (true) {
+            if (!matcher.find()) {
+                break;
+            }
+            String pfull = matcher.group();
+            String jsdirPart = matcher.group(1);
+            pfull = pfull.substring(0, matcher.start(1) - matcher.start()) + resolveStaticAddr(jsdirPart.trim()) + pfull.charAt(pfull.length() - 1);
+            matcher.appendReplacement(sb, Matcher.quoteReplacement(pfull));
+        }
+        matcher.appendTail(sb);
+        return sb.toString();
+    }
 
-	public String resolveImageRefer(String page) {
-		Matcher matcher = IMAGES_PATTERN.matcher(page);
-		StringBuffer sb = new StringBuffer();
+    public String resolveImageRefer(String page) {
+        Matcher matcher = IMAGES_PATTERN.matcher(page);
+        StringBuffer sb = new StringBuffer();
 
-		while (true) {
-			if (!matcher.find()) {
-				break;
-			}
-			String pfull = matcher.group();
+        while (true) {
+            if (!matcher.find()) {
+                break;
+            }
+            String pfull = matcher.group();
 
-			String jsdirPart = matcher.group(1);
-			pfull = pfull.substring(0, matcher.start(1) - matcher.start())
-					+ resolveStaticAddr(jsdirPart.trim())
-					+ pfull.charAt(pfull.length() - 1);
-			matcher.appendReplacement(sb, Matcher.quoteReplacement(pfull));
-		}
-		matcher.appendTail(sb);
-		return sb.toString();
-	}
+            String jsdirPart = matcher.group(1);
+            pfull = pfull.substring(0, matcher.start(1) - matcher.start()) + resolveStaticAddr(jsdirPart.trim()) + pfull.charAt(pfull.length() - 1);
+            matcher.appendReplacement(sb, Matcher.quoteReplacement(pfull));
+        }
+        matcher.appendTail(sb);
+        return sb.toString();
+    }
 
-	public String resolveCssRefer(String page) {
+    public String resolveCssRefer(String page) {
 
-		Matcher matcher = CSS_PATTERN.matcher(page);
-		StringBuffer sb = new StringBuffer();
-		while (matcher.find()) {
-			String pfull = matcher.group();
-			String cssPath = matcher.group(1);
-			pfull = pfull.substring(0, matcher.start(1) - matcher.start())
-					+ resolveStaticAddr(cssPath.trim())
-					+ pfull.charAt(pfull.length() - 1);
-			matcher.appendReplacement(sb, Matcher.quoteReplacement(pfull));
-		}
-		matcher.appendTail(sb);
-		return sb.toString();
-	}
+        Matcher matcher = CSS_PATTERN.matcher(page);
+        StringBuffer sb = new StringBuffer();
+        while (matcher.find()) {
+            String pfull = matcher.group();
+            String cssPath = matcher.group(1);
+            pfull = pfull.substring(0, matcher.start(1) - matcher.start()) + resolveStaticAddr(cssPath.trim()) + pfull.charAt(pfull.length() - 1);
+            matcher.appendReplacement(sb, Matcher.quoteReplacement(pfull));
+        }
+        matcher.appendTail(sb);
+        return sb.toString();
+    }
 
-	private String resolveStaticAddr(String addr) {
-		String newURL = "";
-		addr = addr.trim();
-		if (addr.startsWith("/")) {
-			newURL = resourcesURL + addr;
-		} else if (addr.indexOf("http://") != -1) {
-			newURL = addr;
-		} else {
-			newURL = resourcesURL + "/" + addr;
-		}
+    private String resolveStaticAddr(String addr) {
+        String newURL = addr.trim();
+        if (StringUtils.isNotBlank(resourcesURL)) {
+            if (newURL.startsWith("/")) {
+                newURL = resourcesURL + newURL;
+            } else if (newURL.indexOf("http://") != -1) {
 
-		if (newURL.contains("?")) {
-			newURL += "&v=" + version;
-		} else {
-			newURL += "?v=" + version;
-		}
+            } else {
+                newURL = resourcesURL + "/" + newURL;
+            }
+        }
 
-		return newURL;
-	}
+
+        if (newURL.contains("?")) {
+            newURL += "&v=" + version;
+        } else {
+            newURL += "?v=" + version;
+        }
+
+        return newURL;
+    }
 }
